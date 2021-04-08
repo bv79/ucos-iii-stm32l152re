5985f360aeedf900dd20ff1f9bff068e36e8f2db
bv79 <baovu79@protonmail.com>
2021-04-08 18:35:37 +0300

    include headers

--- template/Inc/main.h
+++ template/Inc/main.h
@@ -32,7 +32,21 @@ extern "C" {
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 
+#include <cpu.h>
+#include <lib_def.h>
+#include <lib_ascii.h>
+#include <lib_math.h>
+#include <lib_mem.h>
+#include <lib_str.h>
+#include <app_cfg.h>
+#include <os_cfg_app.h>
+
+#include <os.h>
 /* USER CODE END Includes */
 
 /* Exported types ------------------------------------------------------------*/
